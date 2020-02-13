8c675bfbe09ee01ee03f4ce72b346212c37ba40a
develop48 <develop48@menbanoiMac.local>
2020-02-13 15:00:30 +0900

    change run

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ git clone https://github.com/tomasr/molokai
 mkdir ~/.vim
 mkdir ~/.vim/colors/
 mv molokai/colors/molokai.vim ~/.vim/colors/
-cp bash_set .bash_profile
-cp input_set .inputrc
+cp bash_set ~/.bash_profile
+cp input_set ~/.inputrc
