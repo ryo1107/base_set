a25be9659a2d26d7d12a0a222b18decec8a1cc81
加藤 諒 <kato_ryo@mac-473.coreprice.com>
2020-06-05 17:10:20 +0900

    実行スクリプトの絶対パス追加

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
 curl https://raw.githubusercontent.com/ryo1107/vimset/master/.vimrc > ~/.vimrc
-git clone https://github.com/tomasr/molokai
+git clone https://github.com/tomasr/molokai $SCRIPT_DIR"/molokai"
 mkdir ~/.vim
 mkdir ~/.vim/colors/
-mv molokai/colors/molokai.vim ~/.vim/colors/
-cp bash_set ~/.bash_profile
-cp input_set ~/.inputrc
+mv $SCRIPT_DIR"/molokai/colors/molokai.vim" ~/.vim/colors/
+cp $SCRIPT_DIR"bash_set" ~/.bash_profile
+cp $SCRIPT_DIR"input_set" ~/.inputrc
